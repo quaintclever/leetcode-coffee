76d17d76aabc26ba647dd48eb1e50ea6bac914f0
quaint <qicong4658>
2021-04-24 18:13:24 +0800

    :memo: leetcode ==> 组合总和 Ⅳ

--- src/com/quaint/leetcode/year2021/April.java
+++ src/com/quaint/leetcode/year2021/April.java
@@ -16,6 +16,29 @@ public class April {
 
 
     /**
+     * 377. 组合总和 Ⅳ
+     * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+     *
+     * 题目数据保证答案符合 32 位整数范围。
+     *
+     *  @param nums
+     * @param target
+     * @return
+     */
+    public int combinationSum4(int[] nums, int target) {
+        int[] fn = new int[target + 1];
+        fn[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    fn[i] += fn[i - num];
+                }
+            }
+        }
+        return fn[target];
+    }
+
+    /**
      * 368. 最大整除子集
      * 给你一个由 无重复 正整数组成的集合 nums ，请你找出并返回其中最大的整除子集 answer ，子集中每一元素对 (answer[i], answer[j]) 都应当满足：
      * answer[i] % answer[j] == 0 ，或
