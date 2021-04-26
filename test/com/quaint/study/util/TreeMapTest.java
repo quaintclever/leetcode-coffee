70e64d610a577515ca07996b0291d6d445883522
quaintclever <857776215@qq.com>
2021-04-26 20:20:21 +0800

    :memo: 测试treemap ==> 测试treemap的排序

---
+++ test/com/quaint/study/util/TreeMapTest.java
@@ -0,0 +1,24 @@
+package com.quaint.study.util;
+
+import java.util.TreeMap;
+
+/**
+ * @author quaint
+ * @date 2021/4/26
+ */
+public class TreeMapTest {
+
+    public static void main(String[] args) {
+
+        // 测试 TreeMap 的排序
+        TreeMap<String, Object> sortMap = new TreeMap<>();
+        sortMap.put("abc", "quaint");
+        sortMap.put("sign", "quaint2");
+        sortMap.put("auth", "quaint3");
+        sortMap.put("name", "quaint4");
+        sortMap.put("age", "quaint5");
+        sortMap.forEach((k, v) -> System.out.println(k + " <- k | v -> " + v));
+
+    }
+
+}
