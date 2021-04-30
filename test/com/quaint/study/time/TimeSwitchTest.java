e6c863e611d01620ed91c76f02da496f96fcbc97
quaintclever <857776215@qq.com>
2021-04-30 11:51:38 +0800

    :memo: test ==> 时间工具类修改名称

--- test/com/quaint/study/time/TimeSwitchTest.java
+++ test/com/quaint/study/time/TimeSwitchTest.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
  * @author quaint
  * @date 2021/4/20
  */
-public class TimeTest extends TimeUtil{
+public class TimeSwitchTest extends TimeSwitchUtil {
 
     public static void main(String[] args) {
         LocalDateTime localDateTime = timeStr2toDayTime("07:11:11");
---
+++ test/com/quaint/study/time/TimeSwitchUtil.java
@@ -0,0 +1,148 @@
+package com.quaint.study.time;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 时间转换工具
+ * @author quaint
+ * @date 2021/4/20
+ */
+public abstract class TimeSwitchUtil {
+
+    public final static String DEFAULT_LOCAL_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public final static String DEFAULT_LOCAL_TIME_FORMAT = "HH:mm:ss";
+
+    /**
+     * LocalDateTime  ==>  毫秒时间戳
+     */
+    public static long switch2milli(LocalDateTime time) {
+        if (time == null) {
+            return 0;
+        }
+        return time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    /**
+     * LocalDate  ==>  时间戳
+     */
+    public static long switch2second(LocalDate date) {
+        if (date == null) {
+            return 0;
+        }
+        LocalDateTime time = date.atTime(0, 0, 0);
+        return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+    /**
+     * LocalDateTime  ==>  时间戳
+     */
+    public static long switch2second(LocalDateTime time) {
+        if (time == null) {
+            return 0;
+        }
+        return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+    /**
+     * LocalDateTime  <==  时间戳
+     */
+    public static LocalDateTime switch4long(long millis) {
+        return Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+
+    /**
+     * LocalDateTime  <==  字符串
+     */
+    public static LocalDateTime switch4string(String timeStr) {
+        return switch4string(timeStr, DEFAULT_LOCAL_DATE_TIME_FORMAT);
+    }
+
+    public static LocalDateTime switch4string(String timeStr, String format) {
+        if (timeStr == null || timeStr.trim().length() == 0) {
+            return null;
+        }
+        try {
+            DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
+            return LocalDateTime.parse(timeStr, df);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("日期字符串不能转换成LocalDateTime!");
+        }
+    }
+
+    /**
+     * LocalDateTime  ==>  string
+     */
+    public static String switch2string(LocalDateTime time) {
+        return switch2string(time, DEFAULT_LOCAL_DATE_TIME_FORMAT);
+    }
+
+    public static String switch2string(LocalDateTime time, String format) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
+        return df.format(time);
+    }
+
+
+    /**
+     * 上面为  基于 LocalDateTime 的转换
+     * =======================================================================
+     * 下面为  str 和 时间戳转换
+     * <p>
+     * 时间戳  ==>  字符串
+     */
+    public static String long2string(long time) {
+        return long2string(time, DEFAULT_LOCAL_DATE_TIME_FORMAT);
+    }
+
+    public static String long2string(long time, String format) {
+        Instant instant = Instant.ofEpochMilli(time);
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, zone);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
+        return df.format(dateTime);
+    }
+
+    /**
+     * 字符串  ==>  时间戳
+     */
+    public static long string2long(String time) {
+        return string2long(time, DEFAULT_LOCAL_DATE_TIME_FORMAT);
+    }
+
+    public static long string2long(String time, String format) {
+        LocalDateTime dateTime = switch4string(time, format);
+        return switch2second(dateTime);
+    }
+
+    /**
+     * 不包含日期的时间字符串, 转为当天的对应时间
+     * @param timeStr
+     * @return
+     */
+    public static LocalDateTime timeStr2toDayTime(String timeStr) {
+        return timeStr2toDayTime(timeStr, DEFAULT_LOCAL_TIME_FORMAT);
+    }
+
+    public static LocalDateTime timeStr2toDayTime(String timeStr, String format) {
+        if (timeStr == null || timeStr.trim().length() == 0) {
+            return null;
+        }
+        try {
+            DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
+            LocalTime lt = LocalTime.parse(timeStr, df);
+            return LocalDate.now().atTime(lt);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("日期字符串不能转换成LocalTime!");
+        }
+    }
+
+    /**
+     * 获取今天 0 时的 时间戳
+     * @return 时间戳
+     */
+    public static long getTodayZeroTime() {
+        return LocalDate.now().atStartOfDay().toEpochSecond(ZoneOffset.of("+8"));
+    }
+}
