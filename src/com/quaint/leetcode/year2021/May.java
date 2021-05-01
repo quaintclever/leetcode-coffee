219e3c05c865a114870abe0482f2b3a72a2e453e
quaintclever <857776215>
2021-05-01 10:15:16 +0800

    :memo: lc ==> 员工的重要性 fork join 写法

--- src/com/quaint/leetcode/year2021/May.java
+++ src/com/quaint/leetcode/year2021/May.java
@@ -3,6 +3,9 @@ package com.quaint.leetcode.year2021;
 import com.quaint.leetcode.util.LcDataStructure;
 
 import java.util.*;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -18,11 +21,39 @@ public class May extends LcDataStructure {
     /**
      * 690. 员工的重要性
      */
+    class MyTask extends RecursiveTask<Integer> {
+
+        Map<Integer, Employee> map;
+        int sum;
+        int id;
+
+        public MyTask(int sum, int id, Map<Integer, Employee> map) {
+            this.sum = sum;
+            this.id = id;
+            this.map = map;
+        }
+
+        @Override
+        protected Integer compute() {
+            Employee employee = map.get(id);
+            sum += employee.importance;
+            employee.subordinates.forEach(i -> sum += new MyTask(0, i, map).fork().join());
+            return sum;
+        }
+    }
+
+    public int getImportanceForkJoin(List<Employee> employees, int id) {
+        Map<Integer, Employee> map = employees.stream().parallel()
+                .collect(Collectors.toMap(e -> e.id, Function.identity()));
+        ForkJoinPool fj = new ForkJoinPool();
+        return fj.invoke(new MyTask(0, id, map));
+    }
+
     public int getImportance(List<Employee> employees, int id) {
         if (employees == null) {
             return 0;
         }
-        Map<Integer, Employee> map = employees.stream().collect(Collectors.toMap(e -> e.id, e -> e));
+        Map<Integer, Employee> map = employees.stream().collect(Collectors.toMap(e -> e.id, Function.identity()));
         int ans = 0;
         // Set<Integer> cache = new HashSet<>();
         Deque<Integer> stk = new LinkedList<>();
